2552e4b91448a14983af41672f72163fb52dde8e
Kiran Anto <kirananto@gmail.com>
2018-10-06 13:56:37 +0530

    render logic for nav drawer done

--- modules.d.ts
+++ modules.d.ts
@@ -1,3 +1,4 @@
 declare module '@atlaskit/layer-manager'
 declare module '@atlaskit/navigation'
 declare module '@atlaskit/icon'
+declare module '@atlaskit/tooltip'
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import LayerManager from '@atlaskit/layer-manager'
 import Navigation, {
   AkNavigationItem,
   AkContainerNavigationNested,
+  presetThemes,
+  AkContainerTitle
 } from '@atlaskit/navigation'
 import * as React from 'react';
 import TrayIcon from '@atlaskit/icon/glyph/tray';
 import JiraLabsIcon from '@atlaskit/icon/glyph/jira/labs';
+import CreateIcon from '@atlaskit/icon/glyph/add';
+import Tooltip from '@atlaskit/tooltip';
+import EditorLayoutThreeEqualIcon from '@atlaskit/icon/glyph/editor/layout-three-equal';
+import TableIcon from '@atlaskit/icon/glyph/table';
+import ArrowLeftIcon from '@atlaskit/icon/glyph/arrow-left';
 import './App.css';
 
 class App extends React.Component<any, any> {
@@ -13,13 +20,22 @@ class App extends React.Component<any, any> {
     super(props);
 
     this.state = {
+      width: 304,
       stack: [
         [
           {
-            title: 'Foo',
+            title: 'autotrader',
+            type: 'database',
             children: [
               {
-                title: 'bar',
+                title: 'at_buyer_details',
+                type: 'table',
+                children: [
+                  {
+                    title: 'Columns',
+                    type: 'column'
+                  }
+                ]
               },
             ],
           },
@@ -27,6 +43,8 @@ class App extends React.Component<any, any> {
       ],
     };
   }
+
+  public handleResize = (pr: { isOpen: boolean, width: number }) => this.setState(pr);
 
   public stackPush = (item: any) => {
     const stack = [...this.state.stack, item];
@@ -41,7 +59,7 @@ class App extends React.Component<any, any> {
   };
 
   public renderBackButton() {
-    return <AkNavigationItem onClick={this.stackPop} text="Back" key="Back" />;
+    return <AkNavigationItem icon={<ArrowLeftIcon label="back" />} onClick={this.stackPop} text="Back" key="Back" />;
   }
 
   public renderHeader = () => {
@@ -51,14 +69,30 @@ class App extends React.Component<any, any> {
       items.push(this.renderBackButton());
     }
 
-    return items;
+    return [<Tooltip key="1" position="right" content="MySQL Workbench">
+      <AkContainerTitle
+        icon={this.state.stack.length > 1 ? <ArrowLeftIcon label="back" /> : <JiraLabsIcon size="large" label="Atlassian" />} 
+        onClick={this.state.stack.length > 1 && this.stackPop}
+        text="MySQL Workbench"
+      />
+    </Tooltip>];
   };
 
   public renderItem = (item: any) => {
     const onClick = item.children && (() => this.stackPush(item.children));
+    let icon
+    switch (item.type) {
+      case 'database': icon = <TrayIcon label="db" />
+        break;
+      case 'table': icon = <TableIcon label="db" />
+        break;
+      case 'column': icon = <EditorLayoutThreeEqualIcon label="db" />
+        break;
+      default: icon = undefined
+    }
 
     return (
-      <AkNavigationItem icon={item.children && <TrayIcon label="db"/>} text={item.title} onClick={onClick} key={item.title} />
+      <AkNavigationItem icon={icon} text={item.title} onClick={onClick} key={item.title} />
     );
   };
 
@@ -69,18 +103,29 @@ class App extends React.Component<any, any> {
     return (
       <LayerManager>
         <div>
-          <Navigation 
-          globalPrimaryIcon={
+          <Navigation
+            globalTheme={presetThemes['siteSettings']}
+            containerTheme={presetThemes['container']}
+            width={this.state.width}
+            onResize={this.handleResize}
+            // containerHeaderComponent={NavigationTitle}
+            globalCreateIcon={
+              <Tooltip key="1" position="right" content="Create new table">
+                <CreateIcon label="search" />
+              </Tooltip>
+            }
+            globalPrimaryIcon={
               <JiraLabsIcon size="large" label="Atlassian" />
             }
-          containerHeaderComponent={this.renderHeader}>
+            containerHeaderComponent={this.renderHeader}
+          >
             <AkContainerNavigationNested
               stack={this.renderStack()}
               onAnimationEnd={(...args: any[]) => console.log('animation end', args)}
             />
           </Navigation>
           <div className="App">
-            <span/>
+            <span />
           </div>
         </div>
       </LayerManager>
